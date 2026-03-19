5402dd690175e6aa6c33e37360399bf73f9b305a
ryanadolph7 <129120582+ryanadolph7@users.noreply.github.com>
2026-03-19 14:15:14 -0600

    Delete c_fft_visualizer directory

--- c_fft_visualizer/include/render.h
+++
@@ -1,9 +0,0 @@
-#ifndef RENDER_H
-#define RENDER_H
-
-#include "redefines.h"
-#include "spectrum.h"
-
-void render_draw(const spectrum_state_t *s, i32 cursor_lock_enabled, i32 cursor_locked_index, i32 cursor_hover_index, i32 show_paused_overlay);
-
-#endif
